f143d1241e674ae54eaca2e9a6007626648c61f9
mattyu123 <matthewyu100@gmail.com>
2023-05-29 19:42:48 -0400

    create function that gives long and lat based on IP address

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //runs our main function
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+//Function that returns an IP address
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+//Function that takes the IP address and returns the coordinates of the location
+fetchCoordsByIP("99.235.52.32", (error, coordinates) => {
+  if(error) {
+    console.log("It didn't work", error)
   }
+  console.log("It worked! Coordinates are:", coordinates);
+})
 
-  console.log('It worked! Returned IP:' , ip);
-});
--- iss.js
+++ iss.js
@@ -1,24 +1,50 @@
 //fetch the API data from each endpoint
-
 const request = require('request');
-const apiURL = 'https://api.ipify.org?format=json'
+const ipURL = 'https://api.ipify.org?format=json';
+
 
 const fetchMyIP = (callback) => {
-  request(apiURL, (error, response, body) => {
+  request(ipURL, (error, response, body) => {
     if (error) {
-      return callback(error)
+      return callback(error);
     }
 
     //check to see if there is an issue with the response code when we attempt to pull the data
     if (response.statusCode !== 200) {
-      const msg = `Status code ${response.statusCode} when fetching IP. Response ${body}`
-      callback(null, Error(msg));
+      const msg = `Status code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
       return;
     }
 
+    //if there are no errors and the status code doesn't raise any alarms, we move forward with the rest of it
+    const data = JSON.parse(body).ip;
+    callback(null, data);
+  });
+};
+
+//function that calls API which returns the coordinates of an IP address
+const fetchCoordsByIP = (ip, callback = () => {}) => {
+  const geoLoc = `http://ipwho.is/${ip}` //manually inputted the IP address to test
+  
+  request(geoLoc, (error, response, body) => {
+    //parse the data and then store the long and lat coordinates in an object
     const data = JSON.parse(body)
-    callback(null, data)
+
+    if(error) {
+      callback(error, null);
+      return; 
+    }
+
+    //Checking to see if a valid status code is being returned
+    if (!data.success) {
+      const msg = `Success status was ${data.sucess}. Server says ${data.message} when searching for IP address ${ip}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const {latitude, longitude} = data
+
+    callback(null,{latitude, longitude})
   })
 }
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };

abe82302c4f6d43745446a3b06896a989ad078d6
mattyu123 <matthewyu100@gmail.com>
2023-05-30 09:17:30 -0400

    run code through linter

--- index2.js
+++ index2.js
@@ -3,8 +3,8 @@ const { nextISSTimesForMyLocation } = require('./iss_promised.js');
 //Call the function that chains all our other functions together and print out the result
 nextISSTimesForMyLocation()
   .then((bofa) => {
-    console.log(bofa)
+    console.log(bofa);
   })
   .catch(error => {
-    console.log("YOU MADE AN ERROR DUMB BITCH:", error.message);
+    console.log("Error:", error.message);
   });
--- iss_promised.js
+++ iss_promised.js
@@ -1,22 +1,22 @@
-const request = require('request-promise-native')
+const request = require('request-promise-native');
 
 //Function to get the IP address of the user
 const fetchMyIP = function() {
-  return request('https://api.ipify.org?format=json')
+  return request('https://api.ipify.org?format=json');
 };
 
 //Get the longitude and latitude based on the IP address
 const fetchCoordsByIP = function(body) {
-  const data = JSON.parse(body).ip
-  return request(`http://ipwho.is/${data}`)
-}
+  const data = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${data}`);
+};
 
 //Get the latitude and longitude coordinates based on IP address
 const fetchISSFlyOverTimes = function(body) {
-  const {latitude, longitude} = JSON.parse(body)
-  const flyover = `https://iss-flyover.dickherokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
-  return request(flyover)
-}
+  const {latitude, longitude} = JSON.parse(body);
+  const flyover = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(flyover);
+};
 
 //Chain all our functions together
 const nextISSTimesForMyLocation = function() {
@@ -24,9 +24,9 @@ const nextISSTimesForMyLocation = function() {
     .then(fetchCoordsByIP)
     .then(fetchISSFlyOverTimes)
     .then(body => {
-      const data = JSON.parse(body)
+      const data = JSON.parse(body);
       return data.response;
-    })
-}
+    });
+};
 
 module.exports = { nextISSTimesForMyLocation };
